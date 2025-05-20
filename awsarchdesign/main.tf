f41b550b728e11d354d7c78a9faa81826f9c05ad
NetradipMohite <168061515+NetradipMohite@users.noreply.github.com>
2025-05-20 10:50:55 +0530

    terraform_snippest

---
+++ awsarchdesign/main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "main_vpc" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "main-vpc"
+  }
+}
+
+# Add subnets, ALB, EC2 ASG, RDS, SGs, etc. here as needed...
